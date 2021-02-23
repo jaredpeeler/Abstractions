7ce5a0ea2c5b29f8b66deb4dca1f113c62caa368
ds-lxteam <59637215+ds-lxteam@users.noreply.github.com>
2021-02-23 10:19:10 -0700

    redo toc

--- docfx_project/templates/ds_temp/toc.extension.js
+++ docfx_project/templates/ds_temp/toc.extension.js
@@ -4,24 +4,7 @@
  * This method will be called at the start of exports.transform in toc.html.js
  */
 exports.preTransform = function (model) {
-  // replace recursively the root namespace by ""
-  transformItem(model, 1);
   return model;
-
-  function transformItem(item, level) {
-    if (item.name) {
-      item.name = item.name.replace("DirectScale.Disco.", '');
-    } else {
-      item.name = null;
-    }
-
-    if (item.items && item.items.length > 0) {
-      var length = item.items.length;
-      for (var i = 0; i < length; i++) {
-        transformItem(item.items[i], level + 1);
-      };
-    }
-  }
 }
 
 /**
